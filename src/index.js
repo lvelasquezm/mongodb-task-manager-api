5b06531a60fcac9d1eecdad33ba25b5989e04887
Lawrence Velasquez <law.vzmk@gmail.com>
2019-10-28 17:13:14 -0600

    Add DELETE endpoints to remove users and tasks

--- src/index.js
+++ src/index.js
@@ -60,14 +60,10 @@ app.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true
-      }
-    );
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
     if (!user) {
       return res.status(400).send();
@@ -76,6 +72,20 @@ app.patch('/users/:id', async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    return res.send(user);
+  } catch {
+    return res.status(400).send();
   }
 });
 
@@ -129,14 +139,10 @@ app.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true
-      }
-    );
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
     if (!task) {
       return res.status(400).send();
@@ -145,6 +151,20 @@ app.patch('/tasks/:id', async (req, res) => {
     res.send(task);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    return res.send(task);
+  } catch {
+    return res.status(400).send();
   }
 });
 
