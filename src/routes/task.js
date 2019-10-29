46144f6fa03550336a04592976a18f73acd6819b
Lawrence Velasquez <law.vzmk@gmail.com>
2019-10-29 16:15:39 -0600

    Add option to filter tasks by the completed field

--- src/routes/task.js
+++ src/routes/task.js
@@ -6,9 +6,16 @@ import authMiddleware from '../middlewares/auth';
 
 const router = new Router();
 
+// GET /tasks?completed=true
 router.get('/tasks', authMiddleware, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({ path: 'tasks', match }).execPopulate();
     res.send(req.user.tasks);
   } catch {
     res.status(500).send();
